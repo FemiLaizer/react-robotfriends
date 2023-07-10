07ebef8dc3e1120b937b4061b971d4debd17e09b
Femi Laizer <femilaizer@gmail.com>
2023-07-10 21:41:48 +0100

    first robot card

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Card.css';
 
-function Card() {
+function Card({ id, name, email }) {
   return (
-    <div className="">
-      <img src='' className="" alt="robot" />
-
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
     </div>
   );
 }
