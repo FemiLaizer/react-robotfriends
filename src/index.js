a5ae91d7c5fe72c3eb12cde1877c3e6b944ddbb9
Femi Laizer <femilaizer@gmail.com>
2023-07-10 21:10:41 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import "./tachyon"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card />
+    <Card />
+    <Card />
   </React.StrictMode>
 );
 
