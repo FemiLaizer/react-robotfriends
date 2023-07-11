bfcf4bb372b6495369ac8a2fa550bc81b9cb20df
Femi Laizer <femilaizer@gmail.com>
2023-07-11 04:31:18 +0100

    added searchbox working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import CardList from "./CardList";
 import { robots } from './robots';
 import SearchBox from "./SearchBox";
 
 const App = () => {
+
+    const [searchField, setSearchField] = useState(robots)
+
+    const searchChange = (e) => {
+        const searchedRobot = robots.filter(user =>
+            user.name.toLowerCase().includes(e.toLowerCase())
+        )
+
+        // console.log(searchedRobot)
+        setSearchField(searchedRobot)
+    }
+
     return (
         <div className="tc">
             <h1>RobotFriend</h1>
-            <SearchBox />
-            <CardList robots={robots} />
+            <SearchBox onSearch={searchChange} />
+            <CardList robots={searchField} />
         </div>
     )
 }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ onSearch }) => {
     return (
         <div className="pa2">
-            <input
+            <input onChange={(e) => onSearch(e.target.value)}
                 className="pa3 ba b--green bg-lightest-blue"
                 type="search"
                 placeholder="search robots" />
